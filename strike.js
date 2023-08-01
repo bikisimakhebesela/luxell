3de6b59bb7e0aee937df917c5a0f745bd3859b9d
bikisimakhebesela <141127361+bikisimakhebesela@users.noreply.github.com>
2023-08-01 11:33:13 +0200

    Add files via upload

---
+++ strike.js
@@ -0,0 +1,14 @@
+var sn = document.getElementById("suf");
+var ln = document.getElementById("lif");
+var ws = document.getElementById("workspace");
+var pdb = document.getElementById("privat");
+var p1 = document.getElementById("p1");
+var op = document.getElementById("options");
+
+pdb.addEventListener('click' ,function (){
+    //ws.appendChild(sn);
+    ws.appendChild(op);
+    p1.style.display="none";
+    op.style.display="block";
+    //ln.style.display="block";
+} );
